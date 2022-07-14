f31146ce9d276ba6185fb18f0657b02917465cb1
iury simoes-sousa <simoesiury@gmail.com>
2022-07-14 13:01:59 -0400

    setting parameters for the runs

---
+++ param_dns.py
@@ -0,0 +1,41 @@
+"""Parameters file."""
+
+import numpy as np
+
+# Domain
+L = 2 * np.pi
+N = 4096
+mesh = None
+
+# Stochastic forcing
+ε = 1  # Energy injection rate
+kf = 32  # Forcing wavenumber
+kfw = 2  # Forcing width
+seed = None
+
+# Physical parameters
+# Prescribed
+lν_kmax = 3  # Enstrophy dissipation scale
+lα = L  # Friction scale
+# Derived
+kmax = N * np.pi / L
+lν = lν_kmax / kmax
+η = ε * kf**2
+ν = lν**2 * η**(1/3)
+α = ε**(1/3) * lα**(-2/3)
+
+# Temporal parameters
+dx = L / N
+Uα = ε**(1/3) * lα**(1/3)
+safety = 0.5
+dt = safety * dx / Uα
+ts = "RK443"
+stop_sim_time = np.inf
+stop_wall_time = 60*60*2
+scalars_iter = 10
+snapshots_iter = 10
+stop_iteration = np.inf
+
+# Noise parameters for ensembles (add to ψ)
+amp = 1e-6
+#-6, -8, -10, -12
