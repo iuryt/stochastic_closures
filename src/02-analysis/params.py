a13257848bb56f6e91e3153cd118f714581e200d
iury simoes-sousa <iuryt@pm.me>
2022-08-01 19:37:47 -0400

    parameters for analysis of the outputs

--- src/02-analysis/params.py
+++ src/02-analysis/params.py
@@ -4,3 +4,19 @@
 
 task = "ψ" # variable to be analyzed
 kmax = 1000 # maximum wavenumber to be used
+
+# Define noise runs that will be analyzed
+
+#noise_runs = ["noise00", "noise05", "noise06", "noise07"]
+noise_runs = ["noise07"]
+
+
+# key args for figures
+
+kw = dict(
+    savefig = dict(
+        dpi=300,
+        bbox_inches="tight",
+        facecolor="w",
+    )
+)
