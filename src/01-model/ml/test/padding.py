4485c8f16db366cfbd9c41902fcc748d33fe6539
iury simoes-sousa <iuryt@pm.me>
2022-08-02 13:55:20 -0400

    testing padding from pytorch

--- src/01-model/ml/test/padding.py
+++ src/01-model/ml/test/padding.py
@@ -13,27 +13,32 @@ x,y = np.meshgrid(*(2*[np.linspace(0,2*np.pi,n)]))
 z = (np.sin(x+y)+np.cos(y))
 
 
-l = 21  # kernel size
-pad = (l-1)//2
+ks = 3  # kernel size
+pad = (ks-1)//2
 
 kw_conv = dict(
     in_channels=1,
     out_channels=1,
-    kernel_size=l,
+    kernel_size=ks,
     stride=1,
     padding=pad,
     padding_mode="circular",
-    dtype=torch.float64
+    dtype=torch.float64,
+    bias=False
 )
 
 # Convolution with PyTorch
 m = nn.Conv2d(**kw_conv)
+with torch.no_grad():
+    m.weight[0,0,:] = 0
+    m.weight[0,0,:][0,0] = 1
+    
 zt = torch.DoubleTensor(z[np.newaxis, np.newaxis, :, :])
 zct = m(zt).detach().numpy()[0, 0, :, :]
 
 # Convolution with SciPy
 weights = m.weight.detach().numpy()[0, 0, ::-1, ::-1] # need to flip weights
-zcs = signal.convolve2d(z,weights,boundary="wrap",mode="same")
+zcs = signal.convolve2d(z, weights, boundary="wrap", mode="same")
 
 # Error
 error = (zct-zcs)
