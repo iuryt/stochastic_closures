99098562b0b7cb9dd503d9c6caa878ef99e19c70
iury simoes-sousa <iuryt@pm.me>
2022-08-04 15:35:51 -0400

    function for loading hdf5 files and saving into a netcdf file

--- src/02-analysis/dataloader.py
+++ src/02-analysis/dataloader.py
@@ -3,8 +3,10 @@ import numpy as np
 import h5py
 import xarray as xr
 from tqdm import tqdm
+import os
+from misc import format_string
 
-def load_task_data(path_to_files, task, kmax=500):
+def load_task_data(path_to_files, task, kmax=500, step_task=2):
     # List of files
     fnames = glob(path_to_files)
     # Sort the files
@@ -21,8 +23,8 @@ def load_task_data(path_to_files, task, kmax=500):
             indx = np.abs(kx)<kmax
             indy = np.abs(ky)<kmax
             
-            data.append(file["tasks"][task][:, indx, :][:, :, indy])
-            time.append(file["scales"]["sim_time"][:])
+            data.append(file["tasks"][task][:, indx, :][:, :, indy][::step_task])
+            time.append(file["scales"]["sim_time"][::step_task])
             kx, ky = kx[indx], ky[indy]
 
     data = np.vstack(data)
@@ -36,3 +38,24 @@ def load_task_data(path_to_files, task, kmax=500):
     da = xr.DataArray(data, dims=("time", "kx", "ky"), coords=coords)
     da = da.sortby("kx").sortby("ky")
     return da
+
+def load_save_netcdf(fname, run, params, print_file, invalid_netcdf=True):
+
+    print(f"\n\n{run}", file = print_file)
+    da = load_task_data(fname, params.task, params.kmax, params.step_task)
+    print("Done loading data", file = print_file)
+
+    param_name = format_string(params.task)
+    if invalid_netcdf:
+        ds = xr.Dataset({f"{param_name}":da})
+        ds.to_netcdf(os.path.join(params.data_dir, run + ".nc"), engine="h5netcdf", invalid_netcdf=True)
+    else:
+        ds = xr.Dataset({f"{param_name}_real":da.real, f"{param_name}_imag":da.imag})
+        ds.to_netcdf(os.path.join(params.data_dir, run + ".nc"))
+    print("Done saving netCDF file", file = print_file)
+    
+
+def load_complex_netcdf(fname, params):
+    ds = xr.open_dataset(fname).load()
+    param_name = format_string(params.task)
+    return ds[f"{param_name}_real"]+1j*ds[f"{param_name}_imag"]
