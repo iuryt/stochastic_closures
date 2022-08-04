24c4e04040e05f4bf9b69f200c3bb17efda497c7
iury simoes-sousa <iuryt@pm.me>
2022-08-04 15:37:03 -0400

    centralized function for computing statistics

--- src/02-analysis/statistics.py
+++ src/02-analysis/statistics.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from scipy import signal
-
+import sys
 
 def correlate(x1,x2=None,method="scipy",normalize=True):
     
@@ -59,7 +59,7 @@ def compute_correlation_kbins(da, db, step_bin = 2):
             bij = b[:,i,j]
 
             # calculate the autocorrelation using fft method
-            lags,cij = correlate(aij,bij)
+            _, cij = correlate(aij,bij)
 
             # find the bin indexes
             k = np.abs(kx + 1j*ky)
@@ -85,15 +85,18 @@ def compute_correlation_kbins(da, db, step_bin = 2):
     return C
 
 
-def compute_error_kbins(da, db, step_bin = 2, normalize=True, reset_time=True):
+def apply_func_kbins(da, db, step_bin = 2, normalize=True, reset_time=True, name="error",
+                    func = lambda a,b: np.abs(a-b),
+                    norm_func = lambda E,a: E/np.abs(a),
+                    average=True):
     """
     da,db == (xarray.DataArray,xarray.DataArray)
     """
 
-    E = np.abs(da-db)
+    E = func(da,db)
     
     if normalize:
-        E = E/np.abs(da)
+        E = norm_func(E,da)
         
     if reset_time:
         E = E.assign_coords(time=E.time.values-E.time.values.min())
@@ -107,11 +110,53 @@ def compute_error_kbins(da, db, step_bin = 2, normalize=True, reset_time=True):
         histogram((K*xr.ones_like(E)).rename("k"), bins = kbins, weights=E, dim=["kx","ky"]).rename(k_bin = "k")
     )
     
-    E = E/H
+    if average:
+        E = E/H
  
-    E = E.rename("error")
+    E = E.rename(name)
 
     return E
+
+
+def compute_statistics(da, db, print_file=sys.stderr):   
+    # Create list of statistics to be computed
+    S = []
+
+    print("Computing errors", file = print_file)
+
+    name = f"normalized_error"
+    func = lambda a,b: np.abs(a-b)
+    norm_func = lambda E,a: E/np.abs(a)
+    S.append(apply_func_kbins(da, db, func = func, norm_func=norm_func, name = name))
+
+    name = f"spectrum_of_difference"
+    func = lambda a,b: np.abs(a-b)**2
+    S.append(apply_func_kbins(da, db, func = func, normalize = False, name = name, average = False))
+
+    name = f"difference_of_spectrum"
+    func = lambda a,b: np.abs(np.abs(a)**2-np.abs(b)**2)
+    S.append(apply_func_kbins(da, db, func = func, normalize = False, name = name, average = False))
+    print("Done computing errors", file = print_file)
+
+    print("Computing spectra", file = print_file)
+    
+    func = lambda a,b: (a*np.conj(b)).real
+
+    name = f"cross_spectrum"
+    S.append(apply_func_kbins(da, db, func = func, normalize = False, name = name, average = False))
+    name = f"spectrum_1"
+    S.append(apply_func_kbins(da, da, func = func, normalize = False, name = name, average = False))
+    name = f"spectrum_2"
+    S.append(apply_func_kbins(db, db, func = func, normalize = False, name = name, average = False))
+    print("Done computing spectra", file = print_file)
+
+
+    print("Computing correlations", file = print_file)
+    S.append(compute_correlation_kbins(da, db))
+    print("Done computing correlations", file = print_file)
+
+    return xr.merge(S)
+    
 
 if __name__ == "__main__":
     
