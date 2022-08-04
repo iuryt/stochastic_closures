7baa4b13a24a37616105c9ddffea6d1a8c528725
iury simoes-sousa <iuryt@pm.me>
2022-08-04 15:36:19 -0400

    add data directory and expand noise runs

--- src/02-analysis/params.py
+++ src/02-analysis/params.py
@@ -1,15 +1,16 @@
 
+# Data directory
+data_dir = "../../data/"
 
 # Define the parameters for the analysis
 
 task = "ψ" # variable to be analyzed
 kmax = 1000 # maximum wavenumber to be used
+step_task = 2 # step size for the task variable
 
 # Define noise runs that will be analyzed
-
-#noise_runs = ["noise00", "noise05", "noise06", "noise07"]
-noise_runs = ["noise07"]
-
+noise_runs = ["noise_004", "noise_005", "noise_006", "noise_007", "noise_008", "noise_010", "noise_012"]
+# noise_runs = ["noise_010"]
 
 # key args for figures
 
